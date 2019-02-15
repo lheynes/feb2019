0d7c0c915b4d99e4f030ef2cb2984371d1a0233c
Lindley Heynes <lindley@mukuru.com>
2019-02-15 14:20:49 +0200

    N++ Scripts/GIT Basics (notes).sh

--- N++ Scripts/GIT Basics (notes).sh
+++ N++ Scripts/GIT Basics (notes).sh
@@ -1,3 +1,9 @@
+GIT BASIC WORKFLOW:
+1. Make Changes to File and save
+2. Add file to staging Index with the GIT ADD command (this get the file ready for syncing to remote repo)
+3. Commit the file to the staging index with the GIT COMMIT command (this will commit the changed file to the staging area)
+4. Push the changed file to the remote repo with the GIT PUSH command (this will replicate the changed file from your local working directory to the remote GITHUB repo)
+
 git init              // Initialise local Git Repository
 git add <file>       // Add File to Index (staging area)
 git status          // Check Status of Working Tree
