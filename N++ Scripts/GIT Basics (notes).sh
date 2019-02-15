d93ce49a6aa91b95bfbf55752abd9ef999d0b96c
Lindley Heynes <lindley@mukuru.com>
2019-02-15 18:49:24 +0200

    I removed some more unwanted text from this file

--- N++ Scripts/GIT Basics (notes).sh
+++ N++ Scripts/GIT Basics (notes).sh
@@ -4,12 +4,12 @@ GIT BASIC WORKFLOW:
 3. Commit the file to the staging index with the GIT COMMIT -M "message about changes made" command (this will commit the changed file to the staging area)
 4. Push the changed file to the remote repo with the GIT PUSH command (this will replicate the changed file from your local working directory to the remote GITHUB repo)
 
-git init              // Initialise local Git Repository
-git add <file>       // Add File to Index (staging area)
-git status          // Check Status of Working Tree
-git commit         // Commit changes in Index
-git commit -m 'file name' 'file name' 'file name' // To commit multiple files
-git commit -a     // To  add & commit changes to files in one go
+git init              	// Initialise local Git Repository
+git add <file>       	// Add File to Index (staging area)
+git status          	// Check Status of Working Tree
+git commit -m         // Commit changes in Index
+git commit 	  file name' 'file name' 'file name' // To commit multiple files
+git commit -a     	// To  add & commit changes to files in one go
 git push          // Push to remote Repository
 git pull         // Pull latest from remote Repository
 git clone       // Clone Repository into a new Directory
