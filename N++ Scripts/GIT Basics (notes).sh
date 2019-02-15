204248c8e97a56455d5c43ff5a2805b1eb3a09f1
Lindley Heynes <lindley@mukuru.com>
2019-02-15 13:14:05 +0200

    N++ Scripts/GIT Basics (notes).sh

--- N++ Scripts/GIT Basics (notes).sh
+++ N++ Scripts/GIT Basics (notes).sh
@@ -2,6 +2,8 @@ git init              // Initialise local Git Repository
 git add <file>       // Add File to Index
 git status          // Check Status of Working Tree
 git commit         // Commit changes in Index
+git commit -a     // To commit changes made to ALL FILES within a Directory
+git commit -m 'file name' 'file name' 'file name' //To commit multiple files
 git push          // Push to remote Repository
 git pull         // Pull latest from remote Repository
 git clone       // Clone Repository into a new Directory
